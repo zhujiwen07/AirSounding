2b8d5ac6deebea828f0b12b3321a87c0be2cba32
zhujiwen07 <775433141@qq.com>
2019-11-14 17:12:37 +0800

    1.测试Bufr

--- Bufr/src/test/java/www/cnr7/com/AppTest.java
+++ Bufr/src/test/java/www/cnr7/com/AppTest.java
@@ -5,8 +5,13 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import ucar.nc2.NetcdfFile;
 import ucar.nc2.NetcdfFileWriter;
+import ucar.nc2.iosp.bufr.Message;
+import ucar.nc2.iosp.bufr.tables.BufrTables;
+import ucar.nc2.iosp.bufr.writer.MessageWriter;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Unit test for simple App.
@@ -18,7 +23,12 @@ public class AppTest
      */
     @Test
     public void shouldAnswerWithTrue() throws IOException {
-        NetcdfFile netcdfFile = NetcdfFile.open("D:\\Projects\\2019\\Test9\\data\\bufr/sn.0028.bin");
-        System.out.println(netcdfFile.getDetailInfo());
+
+        /*List<Message> messages = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            messages.add(new Message());
+        }*/
+        /*NetcdfFile netcdfFile = NetcdfFile.open("D:\\Projects\\2019\\Test9\\data\\bufr/sn.0028.bin");
+        System.out.println(netcdfFile.getDetailInfo());*/
     }
 }
---
+++ Bufr/src/test/java/www/cnr7/com/BufrTest.java
@@ -0,0 +1,20 @@
+package www.cnr7.com;
+
+import ucar.nc2.iosp.bufr.BufrIndicatorSection;
+import ucar.nc2.iosp.bufr.BufrIosp2;
+
+/**
+ * @Author zhujiwen
+ * @Date 周四 2019-11-14 11:18
+ * @Version 1.0
+ * @Description
+ **/
+public class BufrTest {
+    public static void main(String[] args) {
+        /**
+         * 第0节
+         */
+        BufrIosp2 bufrIosp2 = new BufrIosp2();
+//        BufrIndicatorSection bufrIndicatorSection = new BufrIndicatorSection();
+    }
+}
